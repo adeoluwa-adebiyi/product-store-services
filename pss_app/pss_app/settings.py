d733f18d06023dd252964d1a4ce0f16676097f81
Adeoluwa Adebiyi <adeoluwaadebiyi@gmail.com>
2020-07-31 11:53:02 +0100

    [update] add django extensions and scripts package

--- pss_app/pss_app/settings.py
+++ pss_app/pss_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apis'
 ]
 
 MIDDLEWARE = [
@@ -77,6 +78,8 @@ WSGI_APPLICATION = 'pss_app.wsgi.application'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = "apis.User"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
